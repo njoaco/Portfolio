665ef555657c95b36392c895c757f64a4e7d51bd
njoaco <joaquinlucasnieto@gmail.com>
2024-04-12 13:04:28 -0300

    portfolio projects added

---
+++ src/data/portfolioProjects.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        title: 'Sistema de Gestión de Farmacia',
+        imgUrl: '/assets/farmacia.jpg',
+        stack: ['Java', 'mySQL'],
+        link: 'https://github.com/njoaco/Gestion-de-farmacia',
+    },
+    {
+        title: 'AuroraAI',
+        imgUrl: '/assets/auroraAI.jpg',
+        stack: ['JavaScript', 'Tailwind', 'React', 'Expo'],
+        link: 'https://github.com/njoaco/AuroraAI',
+    },
+    {
+        title: 'Weatherify',
+        imgUrl: '/assets/weather.jpg',
+        stack: ['JavaScript', 'Tailwind', 'React', 'Expo'],
+        link: 'https://github.com/njoaco/WeatherApp',
+    },
+    {
+        title: 'Brazo Robotico',
+        imgUrl: '/assets/brazorobotico.jpg',
+        stack: ['C++', 'MIT App Inventor'],
+        link: 'https://github.com/njoaco/BrazoRobotico-Arduino',
+    },
+]
