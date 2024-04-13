8d8676c4ecdfb0cfc0ce1665b8dcac5705bc4572
njoaco <joaquinlucasnieto@gmail.com>
2024-04-13 11:58:54 -0300

    SEO details added

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,7 @@ function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
         <h1 className="text-4xl md:text-6xl mb-1 md:mb-3 font-bold float-in-space" style={{ fontFamily: 'DM Sans, sans-serif' }}>Joaquín Lucas Nieto</h1>
-            <p className="text-base md:text-xl mb-3 font-medium" style={{ fontFamily: 'DM Sans, sans-serif' }}>Desarrollador Full Stack Junior</p>
+            <p className="text-base md:text-xl mb-3 font-medium" style={{ fontFamily: 'DM Sans, sans-serif' }}>@njoaco - Desarrollador Full Stack Junior</p>
             <p className="text-sm max-w-xl mb-6 font-bold" style={{ fontFamily: 'DM Sans, sans-serif' }}>Soy un desarrollador full-stack de 19 años, residente en Buenos Aires, Argentina. Actualmente, soy estudiante de Ingeniería en Sistemas y poseo habilidades en Java, JavaScript, HTML, CSS, C++, React, Vite, Git y base de datos SQL, 
             y del lado del diseño Adobe Photoshop y DaVinci Resolve. Apasionado por el desarrollo de soluciones tecnológicas eficientes, busco oportunidades para contribuir y crecer profesionalmente en un entorno innovador.</p>
             <div className="flex justify-center gap-5">
